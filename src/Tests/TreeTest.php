02ed5f6627f11193bea6d47538e39b579e5eb9cb
Vladislavs <Vladikuk948@gmail.com>
2023-02-07 11:36:13 +0200

    add Tests

--- src/Tests/TreeTest.php
+++ src/Tests/TreeTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace src\Tests;
 
+use mysql_xdevapi\Exception;
 use src\Tree;
 
 final class TreeTest
@@ -11,13 +12,14 @@ final class TreeTest
         $tree = new Tree(1, 'apple');
         $tree->collectFruit();
         if (!($tree->getFruitCount() >= 40 && $tree->getFruitCount() <= 50)) {
-            echo 'Тест не пройден: collectFruit яблочное дерево' . PHP_EOL;
+            throw new Exception('Тест не пройден: collectFruit яблочное дерево' . PHP_EOL);
         }
 
         $tree = new Tree(2, 'pear');
         $tree->collectFruit();
         if (!($tree->getFruitCount() >= 0 && $tree->getFruitCount() <= 20)) {
-            echo 'Тест не пройден: collectFruit груши' . PHP_EOL;
+            throw new Exception('Тест не пройден: collectFruit груши' . PHP_EOL);
+
         }
     }
 }
